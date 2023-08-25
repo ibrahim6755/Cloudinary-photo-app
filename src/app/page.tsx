8835f2dfd6638d731cae405d6f6b3e49f8ae9bad
ibrahim6755 <ibrahimghani020@gmail.com>
2023-08-25 11:30:59 -0700

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,13 @@ import { CldUploadButton } from 'next-cloudinary';
 import { CldImage } from 'next-cloudinary';
 
 
-// export type UploadResult = {
-//   info: {
-//     public_id: string
-//   },
-//   event: 'success',
+export type UploadResult = {
+  info: {
+    public_id: string
+  },
+  event: 'success',
 
-// }
+}
 export default function Home() {
 
   const [imageId, setimageId] = useState("")
