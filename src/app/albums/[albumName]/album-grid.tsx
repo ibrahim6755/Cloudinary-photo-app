41c2e1c2a51706c22c584c93ffc71f651ca3376c
ibrahim6755 <ibrahimghani020@gmail.com>
2023-10-11 15:08:52 -0700

    changes

--- src/app/albums/[albumName]/album-grid.tsx
+++ src/app/albums/[albumName]/album-grid.tsx
@@ -9,17 +9,17 @@ export default function AlbumGrid({images}:{images:SearchResult[]}){
     return(
         <ImageGrid
         images={images}
-        getImage={(imageData :SearchResult)=>{
-            return(
-                <CloudinaryImage
-                key={imageData.public_id}
-                imageData={imageData}
-                width="400"
-                height="300"
-                alt="an image of something"
-                />
-            )
-        }}
+        // getImage={(imageData :SearchResult)=>{
+        //     return(
+        //         <CloudinaryImage
+        //         key={imageData.public_id}
+        //         imageData={imageData}
+        //         width="400"
+        //         height="300"
+        //         alt="an image of something"
+        //         />
+        //     )
+        // }}
         />
     )
 }
